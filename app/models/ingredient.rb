b317d0acb445044f152c5e34052fea79c1af0f24
ayelen gora <ayegora@hotmail.com>
2024-10-25 12:16:00 +0200

    ingredient recipe and recipe_ingredient models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ApplicationRecord
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :recipes, through: :recipe_ingredients
+
+  # Evitar duplicación de ingredientes
+  validates :name, uniqueness: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients
+
+  # Método para buscar recetas por coincidencia parcial en los ingredientes
+  def self.search_by_ingredient(keyword)
+    joins(:ingredients).where("ingredients.name ILIKE ?", "%#{keyword}%").distinct
+  end
 end
