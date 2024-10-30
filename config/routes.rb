0fc177c4c93fd1323169e52d76db37b768b67864
ayelen gora <ayegora@hotmail.com>
2024-10-30 17:48:33 +0100

    Configure root and catch-all route to serve React app

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
+  get "static/index"
   resources :recipes, only: [ :index, :show ]
 
-  # get "recipes/index"
-  # get "recipes/show"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Define the root path to serve the React app
+  root to: "static#index"
+
+  # Catch-all route for React Router
+  get "*path", to: "static#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
